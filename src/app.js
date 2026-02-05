c17ceaa1b0097680b6cee44a0f491b192b5d14e9
Hariom Badwaya <hariombadvaya8002@gmail.com>
2026-02-06 00:19:17 +0530

    multiple route handler with next

--- src/app.js
+++ src/app.js
@@ -5,37 +5,47 @@ const app = express();
 // app.get("/contact", (req, res)=>{
 //     res.send("this is contact page")
 // })
-info = {
-    naam : " hariom",
-    age : 23
-}
-app.post("/post", (req,res)=>{
-    res.send(info)
-})
-app.get("/get",(req,res)=>{
-    res.send(info)
-})
-
-app.use("/hello",(req, res)=>{
-    res.send("Hello hello hello !");
-})
-// app.use("/post",(req,res)=>{
-//     res.send("this is post api response")
+// info = {
+//     naam : " hariom",
+//     age : 23
+// }
+// app.post("/post", (req,res)=>{
+//     res.send(info)
+// })
+// app.get("/get",(req,res)=>{
+//     res.send(info)
 // })
 
-app.use("/test",(req, res)=>{
-    res.send("Hello from the server !");
-})
+// app.use("/hello",(req, res)=>{
+//     res.send("Hello hello hello !");
+// })
+// // app.use("/post",(req,res)=>{
+// //     res.send("this is post api response")
+// // })
 
-// dynamic routing 
-app.get("/user/:userId/:userName/", (req,res)=>{
-    console.log(req.params);
-    res.send({Name: "Hariom"});
-})
+// app.use("/test",(req, res)=>{
+//     res.send("Hello from the server !");
+// })
 
-app.get("/user", (req,res)=>{
-    console.log(req.query);
+// // dynamic routing 
+// app.get("/user/:userId/:userName/", (req,res)=>{
+//     console.log(req.params);
+//     res.send({Name: "Hariom"});
+// })
+
+// app.get("/user", (req,res)=>{
+//     console.log(req.query);
    
+// })
+
+app.use("/user",(req,res,next)=>{
+    console.log("1st route handler");
+  //  res.send("1st response")
+  next()
+},
+(req, res)=>{
+  console.log("2nd route handler")
+  res.send("2nd response");
 })
 
 app.use("/",(req, res)=>{
